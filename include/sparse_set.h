7174ddfb9a263104cc8f5d7595347bdaa6ad7bd2
Josh <josh.ja.carter@gmail.com>
2025-12-31 21:02:09 +1300

    Improved sparse set: iterators; const refs support, with interior mutability

--- include/sparse_set.h
+++ include/sparse_set.h
@@ -20,7 +20,7 @@ struct Page {
 	static constexpr std::size_t default_size { 128 };
 	static constexpr DenseIndex invalid_index { std::numeric_limits<DenseIndex>::max() };
 
-	DenseIndex operator[](PageIndex page_index) {
+	DenseIndex operator[](PageIndex page_index) const {
 		if (page_to_dense.empty()) return invalid_index;
 
 		assert(page_index < page_to_dense.size() && "Page index exceeds page bounds");
@@ -57,26 +57,26 @@ private:
 	std::vector<DenseIndex> page_to_dense {};
 };
 
+struct ISparseSet {};
 
 template<typename T>
-struct SparseSet {
-
+struct SparseSet : ISparseSet {
 
 private:
-	std::vector<Page> sparse;
-	std::vector<T> dense;
+	std::vector<Page> sparse_entries;
+	mutable std::vector<T> dense_entries;
 	std::unordered_map<DenseIndex, SparseIndex> dense_to_sparse;
 
 	std::size_t page_size;
 	std::size_t max_count;
 
-	std::pair<PageIndex, PageIndex> sparse_to_page_indices(SparseIndex sparse_index) {
+	std::pair<PageIndex, PageIndex> sparse_to_page_indices(SparseIndex sparse_index) const {
 		assert(sparse_index < max_count && "Sparse index exceeds sparse set bounds");
 
 		std::size_t page_number = sparse_index / page_size; // which page does this correspond to?
 		PageIndex page_index = sparse_index % page_size; // which index does this correspond to, inside the given page?
 
-		assert(page_number < sparse.size() && "Sparse index maps to non-existent page");
+		assert(page_number < sparse_entries.size() && "Sparse index maps to non-existent page");
 		assert(page_index < page_size && "Sparse index maps to page index beyond the bounds of a single page");
 
 		return { page_number, page_index };
@@ -86,55 +86,87 @@ public:
 	SparseSet(std::size_t max_count, std::size_t page_size = Page::default_size)
 		: max_count { max_count }, page_size { page_size },
 		// if page_size doesn't divide max_count, need an extra page since integer division truncates fractional components
-		sparse ( max_count / page_size + (max_count % page_size == 0 ? 0 : 1) ) {
+		sparse_entries ( max_count / page_size + (max_count % page_size == 0 ? 0 : 1) ) {
 
 		// This reserve size is rather arbitrary - should be adjusted as needed
 		dense_to_sparse.reserve(page_size);
 	}
 
-	T& operator[](SparseIndex sparse_index) {
+	T& operator[](SparseIndex sparse_index) const {
 		auto [ page_number, page_index ] = sparse_to_page_indices(sparse_index);
 
-		DenseIndex dense_index = sparse[page_number][page_index];
+		DenseIndex dense_index = sparse_entries[page_number][page_index];
 
 		assert(dense_index != Page::invalid_index && "Attempted to access a non-existent entry");
 
-		return dense[dense_index];
+		return dense_entries[dense_index];
+	}
+
+	T& dense(DenseIndex dense_index) const {
+		assert(dense_index < dense_entries.size() && "Attempted to access beyond the end of the dense array");
+
+		return dense_entries[dense_index];
+	}
+
+	std::size_t size() const {
+		return dense_entries.size();
 	}
 
 	void add_entry(SparseIndex sparse_index, T entry) {
 		auto [ page_number, page_index ] = sparse_to_page_indices(sparse_index);
 
-		assert(sparse[page_number][page_index] == Page::invalid_index && "Attempted to add entry, but this sparse index already has one");
+		// There is already an associated entry
+		if (sparse_entries[page_number][page_index] != Page::invalid_index) return;
 
-		DenseIndex dense_index = dense.size();
-		dense.emplace_back(entry);
-		sparse[page_number].add_entry(page_index, dense_index, page_size);
+		DenseIndex dense_index = dense_entries.size();
+		dense_entries.emplace_back(entry);
+		sparse_entries[page_number].add_entry(page_index, dense_index, page_size);
 		dense_to_sparse[dense_index] = sparse_index;
 	}
 
 	void remove_entry(SparseIndex sparse_index0) {
 		// Find the dense index of the entry we want to remove
 		auto [ page_number0, page_index0 ] = sparse_to_page_indices(sparse_index0);
-		DenseIndex dense_index0 = sparse[page_number0][page_index0];
+		DenseIndex dense_index0 = sparse_entries[page_number0][page_index0];
 
-		assert(dense_index0 != Page::invalid_index && "Attempted to remove entry, but this sparse index has none associated");
-		assert(dense.size() > 0 && "Attempted to remove entry, but no entries are currently stored.");
+		// There is no associated sparse entry
+		if (dense_index0 == Page::invalid_index) return;
+
+		assert(dense_entries.size() > 0 && "Attempted to remove entry, but no entries are currently stored.");
 
 		// Find the sparse index of the last dense entry
-		DenseIndex dense_index1 = dense.size() - 1;
+		DenseIndex dense_index1 = dense_entries.size() - 1;
 		assert(dense_to_sparse.contains(dense_index1) && "Final element of dense vector does not have reverse dense_to_sparse mapping.");
 		SparseIndex sparse_index1 = dense_to_sparse[dense_index1];
 		auto [ page_number1, page_index1 ] = sparse_to_page_indices(sparse_index1);
 
 		// Swap the entries, in the dense array
-		dense[dense_index0] = dense[dense_index1];
-		dense.pop_back();
+		dense_entries[dense_index0] = dense_entries[dense_index1];
+		dense_entries.pop_back();
 
 		// Book-keeping with pages; remove the old entries for both sparse indices, then remap the swapped one to the deleted dense index
-		sparse[page_number0].remove_entry(page_index0);
-		sparse[page_number1].remove_entry(page_index1);
-		if (sparse_index0 != sparse_index1) sparse[page_number1].add_entry(page_index1, dense_index0, page_size);
+		sparse_entries[page_number0].remove_entry(page_index0);
+		sparse_entries[page_number1].remove_entry(page_index1);
+		if (sparse_index0 != sparse_index1) sparse_entries[page_number1].add_entry(page_index1, dense_index0, page_size);
+	}
+
+	using iterator = T*;
+	using const_iterator = const T*;
+
+	iterator begin() {
+		return dense_entries.data();
+	}
+
+	iterator end() {
+		return dense_entries.data() + size();
+	}
+
+	const_iterator begin() const {
+		return dense_entries.data();
+	}
+
+	const_iterator end() const {
+		return dense_entries.end() + size();
 	}
 
 	//void print_sparse() {
